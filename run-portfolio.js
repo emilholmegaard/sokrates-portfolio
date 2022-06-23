45f76bf7760f5c7b0b0eed6e8ae22111a027e732
Emil Holmegaard <emilholmegaard@gmail.com>
2022-06-23 12:36:07 +0200

    Update run-portfolio.js
    Update to match the real sokrates landscape process as described at https://www.sokrates.dev/

--- run-portfolio.js
+++ run-portfolio.js
@@ -1,26 +1,19 @@
 const fs = require('fs');
-const { exec } = require('child_process');
+const { execSync, exec } = require('child_process');
 
-const config = JSON.parse(fs.readFileSync('config.json'));
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const sokratesConfigPathAppender = '/_sokrates/config.json';
 const sokratesJarFilePath = config.sokratesJarFilePath;
-const PAT = config.PAT;
+const PAT = config.Base64PAT;
 const gitBaseUrl = config.baseUrl;
+const gitUser = config.defaultUser;
 const javaOptions = config.javaOptions;
-const sokratesPortfolio = config.sokratesPortfolio
-const sokratesLandscapes = config.landscapes
+const sokratesPortfolio = config.sokratesPortfolio;
+const sokratesAnalysis = config.sokratesAnalysisLocation;
+const sokratesLandscapes = config.landscapes;
 
 const ignoreFolders = config.ignoreFoldersForHistory;
 const ignoreFiles = config.ignoreFilesForHistory;
-
-
-const base64EncodePat = function () {
-    usernameAndPAT = ':'+PAT;
-    let buff = new Buffer(usernameAndPAT);
-    return buff.toString('base64');
-};
-
-
 
 const execHelper = function (command) {
     child = exec(command,
@@ -37,18 +30,22 @@ const execHelper = function (command) {
         console.log('child process exited with ' +
             `code ${code} and signal ${signal}`);
     });
+    return true;
+};
+
+const execSyncHelper = function (command) {
+    child = execSync(command);
 
     return true;
 
 };
-
 const sokratesExtractHistory = function (repoPath) {
     execHelper('cd ' + repoPath + ' && java -jar ' + sokratesJarFilePath + ' extractGitHistory');
 };
 
 const sokratesInit = function (repoPath) {
     if (!fs.existsSync(repoPath + sokratesConfigPathAppender)) {
-        execHelper('cd ' + repoPath + ' && java -jar ' + sokratesJarFilePath + ' init');
+        execSyncHelper('cd ' + repoPath + ' && java -jar ' + sokratesJarFilePath + ' init');
     }
 };
 
@@ -56,6 +53,10 @@ const sokratesGenerateReport = function (repoPath) {
     execHelper('cd ' + repoPath + ' && java -jar ' + javaOptions + ' ' + sokratesJarFilePath + ' generateeports');
 };
 
+
+const moveResultsToLandscape = function (analysisPath, landscapePath) {
+    execSyncHelper('cp '+ analysisPath + '/_sokrates '+ landscapePath);
+};
 
 const sokratesUpdateLandscape = function (landscape) {
     execHelper('cd ' + sokratesPortfolio + '/' + landscape + ' && java -jar ' + sokratesJarFilePath + ' updateLandscape')
@@ -76,34 +77,40 @@ const optimizeForLandscape = function (repoPath, landscape) {
     cleanFileIgnores(repoPath);
 };
 
-const getSourceCode = function (repo, landscape) {
-    let repoPath = sokratesPortfolio + '/' + landscape + '/' + repo.split('/').pop();
+const getSourceCode = function (repo, landscape, analysisPath, landscapePath) {
+
     if (!fs.existsSync(sokratesPortfolio + '/' + landscape)) {
         fs.mkdirSync(sokratesPortfolio + '/' + landscape);
     }
 
-    if (!fs.existsSync(repoPath)) {
-        let repository = 'https://' + gitBaseUrl + '/' + repo;
-        execHelper('cd ' + sokratesPortfolio + '/' + landscape + ' && git -c http.extraHeader="Authorization: Basic '+ base64EncodePat() +'" clone ' + repository);
+    if (!fs.existsSync(analysisPath)) {
+        let repository = 'https://'+ gitUser + '@' + gitBaseUrl + '/' + repo;
+        let cloneCommand = 'git -c http.extraHeader="Authorization: Basic '+ PAT +'" clone ' + repository;
+	console.log(cloneCommand);
+	execSyncHelper('cd ' + sokratesAnalysis + ' && ' + cloneCommand);
     } else {
-        execHelper('cd ' + repoPath + ' && git pull');
+        execSyncHelper('cd ' + analysisPath + ' && git -c http.extraHeader="Authorization: Basic '+ PAT +'" pull');
     }
 };
 
 const updatePortfolio = function () {
     for (const landscape of sokratesLandscapes) {
         for (const repository of landscape.repositories) {
-            let repoPath = sokratesPortfolio + '/' + landscape.name + '/' + repository.split('/').pop();
-            console.log(repoPath);
-            getSourceCode(repository, landscape.name);
-            sokratesExtractHistory(repoPath);
-            sokratesInit(repoPath);
-            optimizeForLandscape(repoPath);
-            sokratesGenerateReport(repoPath);
-            sokratesUpdateLandscape(landscape.name)
+            let landscapePath = sokratesPortfolio + '/' + landscape.name + '/' + repository.split('/').pop();
+            let analysisPath = sokratesAnalysis + '/' + repository.split('/').pop();
+            console.log("lanscapePath: "+landscapePath);
+	        console.log("analysisPath: "+analysisPath);
+            getSourceCode(repository, landscape.name, analysisPath, landscapePath);
+            sokratesExtractHistory(analysisPath);
+            sokratesInit(analysisPath);
+            optimizeForLandscape(analysisPath);
+            sokratesGenerateReport(analysisPath);
+     	    execSync('sleep 5000');
+	        moveResultsToLandscape(analysisPath, landscapePath);
         }
-        sokratesUpdateLandscape("");
-    }
+	    sokratesUpdateLandscape(landscape.name);
+        }
+	sokratesUpdateLandscape("");
 }
 
 
